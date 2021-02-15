c06d38a7765224230d0bd38189936771a8f49fa0
vasya <qq>
2021-02-15 11:07:21 +0300

    hgvbghv

--- oap_labs/Program.cs
+++ oap_labs/Program.cs
@@ -10,6 +10,44 @@ namespace oap_labs
     {
         static void Main(string[] args)
         {
-        }
+            Console.Write("Input number6: ");
+            var number6 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input number9: ");
+            var number9 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input number12: ");
+            var number12 = Convert.ToDouble(Console.ReadLine());
+            if (number6 >= 0)
+            {
+                number6 = Math.Pow(number6, 9);
+            }
+            else
+            {
+                number6 = Math.Pow(number6, 9);
+            }
+            if (number9 >= 0)
+            {
+                number9 = Math.Pow(number9, 9);
+            }
+            else
+            {
+                number9 = Math.Pow(number9, 12);
+            }
+            number9 = (number12 >= 0) ? Math.Pow(number12, 9) : Math.Pow(number12, 15);
+            if (number9 >= 0)
+            {
+                number9 = Math.Pow(number12, 9);
+            }
+            else
+            {
+                number9 = Math.Pow(number12, 15);
+            }
+            Console.WriteLine($"number6 = {number6}");
+            Console.WriteLine($"number9 = {number9}");
+            Console.WriteLine($"number12 = {number12}");
+            {
+                /*
+                /*
+            
+            }
     }
 }
